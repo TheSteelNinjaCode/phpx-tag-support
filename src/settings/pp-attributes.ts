81d8b69dcf81fd839701238eb19d1387e482525f
TheSteelNinjaCode <thesteelninjacode@gmail.com>
2025-10-11 23:46:49 -0600

    feat: add new attributes for loading content and reference handling in pp-attributes

--- src/settings/pp-attributes.ts
+++ src/settings/pp-attributes.ts
@@ -9,6 +9,21 @@ interface AttrDoc {
 
 export const ATTRS: readonly AttrDoc[] = [
   {
+    label: 'pp-loading-content="true"',
+    blurb:
+      "A loading screen is a user interface displayed while a route is loading. This file, loading.php, defines the content shown during the loading process.",
+    mdExtra: `[Full docs](https://prismaphp.tsnc.tech/docs?doc=fc-loading)`,
+  },
+  {
+    label: "pp-ref",
+    blurb:
+      "Registers a **ref** so you can grab the element in JS/TS: `const btn = pp.ref('myBtn')`.",
+    mdExtra: `\`\`\`ts
+const modal = pp.ref('modal');
+modal.open();
+\`\`\``,
+  },
+  {
     label: "pp-spread",
     blurb:
       "Spreads an **object** of props / attributes onto the element in a single go.",
@@ -25,7 +40,7 @@ export const ATTRS: readonly AttrDoc[] = [
     mdExtra: `\`\`\`html
 <ul>
   <template pp-for="(item) in items">
-    <li>{{ item.name }}</li>
+    <li>{item.name}</li>
   </template>
 </ul>
 \`\`\`
